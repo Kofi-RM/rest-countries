303e1b2b83591e9357b702e486e9e921a7f74e1a
Kofi-RM <kofireevesmiller@gmail.com>
2026-05-08 01:03:25 -0400

    Added cursor and hover effect to cards.

--- project/rest-countries/src/CountryPanel.tsx
+++ project/rest-countries/src/CountryPanel.tsx
@@ -4,9 +4,9 @@ export default function CountryPanel() {
 
     return (
         <>
-        <div className = "rounded-lg shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)] m-4 flex flex-col w-60">
+        <div className = "transition hover:scale-105 cursor-pointer bg-white rounded-lg shadow-[0px_10px_20px_0px_rgba(0,_0,_0,_0.1)] m-4 flex flex-col w-60">
         <img src = "https://flagcdn.com/ai.svg"></img>
-        <div className="m-2">
+        <div className="m-2 ">
             <p className="m-2"><span>Population:</span> 200</p>
              <p className="m-2"><span>Region:</span> </p>
               <p className="m-2"><span>Capital:</span> </p>
