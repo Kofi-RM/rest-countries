8a4debbfe669c27f546f371d94d9a0dc59ecb816
Kofi-RM <kofireevesmiller@gmail.com>
2026-05-08 08:44:57 -0400

    Added pages at the bottom

--- project/rest-countries/src/HomePage.tsx
+++ project/rest-countries/src/HomePage.tsx
@@ -21,6 +21,13 @@ const filteredCountries = useMemo(() => {
 
 const pageProps = {itemsPerPage: itemsPerPage, initialPage: 1, totalItems: filteredCountries.length}
 const pageValues = usePagination(pageProps)   
+
+const paginatedCountries = useMemo(() => {
+  return filteredCountries.slice(
+    pageValues.startIndex,
+    pageValues.endIndex + 1
+  );
+}, [filteredCountries, pageValues.startIndex, pageValues.endIndex]);
 return (
         <>
         
@@ -28,9 +35,40 @@ return (
     <SearchBar onSearch={setQuery} onRegion={setFilter}/>
     <div className='flex flex-wrap justify-around'>
    
-       {filteredCountries.map((c) => (
+       {paginatedCountries.map((c) => (
         <CountryPanel key = {c.name.common}country={c}/>
         ))}
+        <div className="flex items-center justify-center gap-4 mt-6">
+
+  <button
+    onClick={pageValues.prevPage}
+    disabled={!pageValues.canPrevPage}
+    className="px-4 py-2 rounded shadow disabled:opacity-50"
+    style={{
+      background: "var(--element)",
+      color: "var(--text)",
+    }}
+  >
+    Prev
+  </button>
+
+  <p>
+    Page {pageValues.currentPage} of {pageValues.totalPages}
+  </p>
+
+  <button
+    onClick={pageValues.nextPage}
+    disabled={!pageValues.canNextPage}
+    className="px-4 py-2 rounded shadow disabled:opacity-50"
+    style={{
+      background: "var(--element)",
+      color: "var(--text)",
+    }}
+  >
+    Next
+  </button>
+
+</div>
       </div>
         </>
     )
