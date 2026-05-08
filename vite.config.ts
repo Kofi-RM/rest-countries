153b5721a7dd58cbc776f6fa4a0f55407bcf6fcc
Kofi-RM <kofireevesmiller@gmail.com>
2026-05-08 09:36:19 -0400

    Tried npm build

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: "/rest-countries/",
   plugins: [
     react(),
      tailwindcss(),
